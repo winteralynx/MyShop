bf8eaa15404f53ef5a644c7a241c5255c195f501
Landon Tholen <winteralynx@gmail.com>
2018-02-27 21:27:54 -0700

    Created product categories and ViewModel

--- MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
+++ MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
@@ -4,17 +4,20 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MyShop.Core.Models;
+using MyShop.Core.ViewModels;
 using MyShop.DataAccess.InMemory; 
 
 namespace MyShop.WebUI.Controllers
 {
     public class ProductManagerController : Controller
     {
-        ProductRepository context; 
+        ProductRepository context;
+        ProductCategoryRepository productCategories; 
 
         public ProductManagerController()
         {
-            context = new ProductRepository(); 
+            context = new ProductRepository();
+            productCategories = new ProductCategoryRepository(); 
         }
 
         // GET: ProductManager
@@ -26,8 +29,10 @@ namespace MyShop.WebUI.Controllers
 
         public ActionResult Create()
         {
-            Product product = new Product();
-            return View(product); 
+            ProductManagerViewModel viewModel = new ProductManagerViewModel(); 
+            viewModel.Product = new Product();
+            viewModel.ProductCategories = productCategories.Collection(); 
+            return View(viewModel); 
         }
 
         [HttpPost]
