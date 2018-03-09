0c3fb7778d018798a7fd3352e32b854e0ee2f03e
Landon Tholen <winteralynx@gmail.com>
2018-03-09 09:11:28 -0700

    Implemented generic repository

--- MyShop/MyShop.Core/Models/Product.cs
+++ MyShop/MyShop.Core/Models/Product.cs
@@ -8,10 +8,8 @@ using System.Threading.Tasks;
 
 namespace MyShop.Core.Models
 {
-    public class Product
+    public class Product : BaseEntity
     {
-        public string Id { get; set; }
-
         [StringLength(20)]
         [DisplayName("Product Name")]
         public string Name { get; set; }
@@ -22,10 +20,5 @@ namespace MyShop.Core.Models
         public string Category { get; set; }
         public string Image { get; set; }
 
-        //Constructor
-        public Product()
-        {
-            this.Id = Guid.NewGuid().ToString(); 
-        }
     }
 }
